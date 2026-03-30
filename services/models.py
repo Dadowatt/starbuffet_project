c270e70a5a82b1eb4c86015aa1d6a3bb424ae17c
Dadowatt <dadowatt92@gmail.com>
2026-03-30 16:22:49 +0000

    création du modèle Traiteur et migrations

--- services/models.py
+++ services/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Traiteur(models.Model):
+    nomcomplet = models.CharField(max_length=255)
+    specialites = models.CharField(max_length=255)
+    description = models.TextField()
+    adresse = models.CharField(max_length=255)
+    est_actif = models.BooleanField(default=True)
+    email = models.EmailField()
+    date_de_creation = models.DateTimeField(auto_now_add=True)
+    telephone = models.CharField(max_length=20)
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nomcomplet
