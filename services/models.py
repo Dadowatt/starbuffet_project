93bcf7bed6430f540425db15d30c2bd7fb51cb2f
Dadowatt <dadowatt92@gmail.com>
2026-04-02 10:55:47 +0000

    ajout de la fonctionalité de l’état des traiteurs via le champ est_actif

--- services/models.py
+++ services/models.py
@@ -5,12 +5,11 @@ class Traiteur(models.Model):
     specialites = models.CharField(max_length=255)
     description = models.TextField()
     adresse = models.CharField(max_length=255)
-    est_actif = models.BooleanField(default=True)
+    est_actif = models.BooleanField(default=False)
     email = models.EmailField()
     date_de_creation = models.DateTimeField(auto_now_add=True)
     telephone = models.CharField(max_length=20)
     annees_experience = models.PositiveBigIntegerField(default=0)
-    #image = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='traiteurs/', blank=True, null=True)
 
     def __str__(self):
